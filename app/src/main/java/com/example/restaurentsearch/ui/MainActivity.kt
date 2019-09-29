f16787c0b997ddb0b8d479597f6f541c3b713af4
rahul7838 <rahulsinghal0308@gmail.com>
2019-09-29 09:30:42 +0530

    Main activity - Provide main screen window

---
+++ app/src/main/java/com/example/restaurentsearch/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.restaurentsearch.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.restaurentsearch.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
